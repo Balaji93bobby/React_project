10b2979bccd511e913f3c03046073f87c0f29527
balaji93bobby <balaji93bobby@gmail.com>
2025-08-04 11:04:03 +0530

    practiced loops in js

---
+++ javascript/loops/app.js
@@ -0,0 +1,32 @@
+// DRY don't repeat yourself
+
+// while loop
+// let counter = 1
+
+
+// while (counter <= 5){
+    
+//     console.log(counter)
+//     counter = counter + 1
+// }; 
+
+// for loop
+
+// for (let i = 1; i <= 25; i++){
+//     if(i % 5 === 0){
+//         console.log(`${i} ASAP Frontend`)
+//     }
+//     else if(i % 2 === 0){
+//         console.log(`${i} Frontend`)
+//     }
+//     else{
+//         console.log(`${i} ASAP`)
+//     }
+// }
+
+let str = `ASAP Frontend`
+
+for (let i = 0;i < str.length; ++i){
+    console.log(str[i])
+}
+
