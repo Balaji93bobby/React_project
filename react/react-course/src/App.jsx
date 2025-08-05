9b722618ed076087ff11e8d935ddf1a131d00536
balaji93bobby <balaji93bobby@gmail.com>
2025-08-05 14:24:48 +0530

    created a new spearate component and use dit in app.jsx

--- react/react-course/src/App.jsx
+++ react/react-course/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './components/Todo'
+import TodoTitle from './components/TodoTitle'
 
 function App() {
   return (
     <>
-    <h1>My Todo List</h1>
+    <TodoTitle/>
     <Todo />
     <Todo />
     <Todo />
