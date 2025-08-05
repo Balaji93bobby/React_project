dd8eb7add91895d6c64da9563c0af24ab248f34c
balaji93bobby <balaji93bobby@gmail.com>
2025-08-05 11:44:23 +0530

    practiced using components

--- react/react-course/src/App.jsx
+++ react/react-course/src/App.jsx
@@ -2,27 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './components/Todo'
 
 function App() {
   return (
     <>
     <h1>My Todo List</h1>
-    <div>
-      <h2>Finish React Crash Course</h2>
-      <button>Delete</button>
-    </div>
-    <div>
-      <h2>Finish ASAP Frontend Course</h2>
-      <button>Delete</button>
-    </div>
-    <div>
-      <h2>Land a Job</h2>
-      <button>Delete</button>
-    </div>
-    <div>
-      <h2>Earn 10 LPA</h2>
-      <button>Delete</button>
-    </div>
+    <Todo />
+    <Todo />
+    <Todo />
+    <Todo />
     </>
   )
 }
--- react/react-course/src/components/Todo.jsx
+++ react/react-course/src/components/Todo.jsx
@@ -1,7 +1,8 @@
 function Todo(){
     return(
         <div>
-        Todo Item
+        <h2> Finish React Crash Course</h2>
+        <button>Delete</button>
         </div>
     )
 }
