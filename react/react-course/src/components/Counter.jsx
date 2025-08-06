6cf98df07e949fc6a055ed157971b2311900c981
balaji93bobby <balaji93bobby@gmail.com>
2025-08-06 05:43:34 +0530

    practiced using useState with objects and arrays

--- react/react-course/src/components/Counter.jsx
+++ react/react-course/src/components/Counter.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 
 
 function Counter(){
-    const [countvalue, setCountvalue] = useState(0)
+    // const [countvalue, setCountvalue] = useState(0)
+    // const [user, setUser] = useState({name: 'Balaji', age: 32})
+    const [array, setArray] = useState([1,2,3,4])
 
 
     return(
@@ -10,14 +12,28 @@ function Counter(){
         <div>
             <h1 className="counter__title">
                 Counter:
-                <span className="count">{ countvalue }</span>
+                <span className="count">{ array.toString() }</span>
             </h1> 
             
         </div>
-        <button className="increment__btn" onClick={() => setCountvalue((prevCount) => prevCount + 1)}>Increment</button>
-        <button className="decrement__btn"onClick={() => setCountvalue((prevCount) => prevCount - 1
-        )}>Decrement</button>
-        <button className="reset__btn" onClick={() => setCountvalue(0)}>Reset</button>
+        <button className="increment__btn" 
+        onClick={() => {
+            setArray((prevArray) => [...prevArray, 1])
+        }}
+        >Increment
+        </button>
+        <button className="decrement__btn"
+        onClick={() => {
+            setArray((prevArray) => [...prevArray, -1])
+        }}
+
+        >Decrement</button>
+        <button className="reset__btn"
+        onClick={() => {
+            setArray([])
+        }}
+
+        >Reset</button>
         </>
     )
 }
